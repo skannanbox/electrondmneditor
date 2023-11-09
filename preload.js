1ffb102e9740ad0302746527c54dd9bbdb4d031c
Pi <pi@Saraswathis-MacBook-Air.local>
2023-11-09 23:53:03 +0530

    submitting editor code

---
+++ preload.js
@@ -0,0 +1,28 @@
+/**
+ * The preload script runs before. It has access to web APIs
+ * as well as Electron's renderer process modules and some
+ * polyfilled Node.js functions.
+ *
+ * https://www.electronjs.org/docs/latest/tutorial/sandbox
+ */
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  setTitle: (title) => ipcRenderer.send('set-title', title)
+
+
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type])
+  }
+})
+
+const { contextBridge, ipcRenderer } = require('electron')
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  setTitle: (title) => ipcRenderer.send('set-title', title),
+  save: (xml) => ipcRenderer.send('save', xml),
+  openFile: () => ipcRenderer.invoke('dialog:openFile')
+})
